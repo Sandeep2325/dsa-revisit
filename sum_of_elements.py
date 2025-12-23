5ff7b134e8a60ed2ccbd366e630eaffdd123a8e7
sandeep2325 <sandeepgowda2314@gmail.com>
2025-12-23 13:32:27 +0530

    sum of el

--- sum_of_elements.py
+++ sum_of_elements.py
@@ -1,3 +1,12 @@
-def sum_of_el(list):
-    print(sum(list))
-sum_of_el([3,4])
+# def sum_of_el(list):
+#     print(sum(list))
+# sum_of_el([3,4])
+
+def sum_of_all(arr):
+    total = 0
+
+    for i in arr:
+        total=total+i
+    return total
+
+print(sum_of_all([6,7,10]))
